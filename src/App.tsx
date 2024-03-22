c6c6b34acd797fcf0ca59506f3d36d0bbd1553cd
Sebastian298 <jaimetenoriorios@outlook.com>
2024-03-21 23:24:30 -0500

    Add menu and usage sections to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,18 @@ export const App = () => {
   return (
     <>
       <header className=" bg-teal-400 py-5">
-        <h1 className=" text-center text-4xl font-black">Tip and Consumption Calculator</h1>
+        <h1 className=" text-center text-4xl font-black">
+          Tip and Consumption Calculator
+        </h1>
       </header>
+      <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menu</h2>
+        </div>
+        <div>
+          <h2>Usage</h2>
+        </div>
+      </main>
     </>
   );
 };
