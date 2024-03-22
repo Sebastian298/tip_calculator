5462fb673a8358197ef24c14524c5676f3c76e93
Sebastian298 <jaimetenoriorios@outlook.com>
2024-03-21 23:07:45 -0500

    Creation a header tag in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 export const App = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <header className=" bg-teal-400 py-5">
+        <h1 className=" text-center text-4xl font-black">Tip and Consumption Calculator</h1>
+      </header>
     </>
   );
 };
