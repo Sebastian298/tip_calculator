09897befad71f8e17ec6e9df9f55498616e87ab5
Sebastian298 <jaimetenoriorios@outlook.com>
2024-03-23 00:59:51 -0500

    Add menu items to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import MenuItem from "./components/MenuItem";
+import { menuItems } from "./data/db";
 export const App = () => {
   return (
     <>
@@ -7,8 +9,13 @@ export const App = () => {
         </h1>
       </header>
       <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-        <div>
-          <h2>Menu</h2>
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menu</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map((item) => (
+              <MenuItem key={item.id} item={item} />
+            ))}
+          </div>
         </div>
         <div>
           <h2>Usage</h2>
